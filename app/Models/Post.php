ffa662591e97355f53915b156f61f9eeb051da47
mirkone1994 <mirkochiappori@gmail.com>
2021-10-19 16:08:10 +0200

    added faker

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
+    protected $fillable = ['title', 'content', 'slug', 'image'];
     public function getFormattedDate($column, $format = 'd-m-Y H:i:s'){
         return Carbon::create($this->$column)->format($format);
     }
--- database/seeds/PostTableSeeder.php
+++ database/seeds/PostTableSeeder.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Models\Post;
+use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
 
 class PostTableSeeder extends Seeder
 {
@@ -9,8 +12,15 @@ class PostTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for($i = 0; $i < 50; $i++){
+            $post = new Post();
+            $post->title = $faker->text(50);
+            $post->content = $faker->paragraphs(2, true);
+            $post->image = $faker->imageUrl(360, 360);
+            $post->slug = Str::slug($post->title, '-');
+            $post->save();
+        }
     }
 }
