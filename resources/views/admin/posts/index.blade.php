f575ff00cc6cad44f83eafc8b504df9d55c217ff
mirkone1994 <mirkochiappori@gmail.com>
2021-10-19 16:47:34 +0200

    made button for cruds

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -2,7 +2,11 @@
 
 @section('content')
 <div class="container">
+  <header class="my-5 d-flex justify-content-between align-items-center">
     <h1>I miei post</h1>
+    <a href="{{route('admin.posts.create')}}" class="btn btn-success">Nuovo post</a>
+
+  </header>
     <table class="table">
         <thead>
           <tr>
@@ -16,7 +20,15 @@
             <tr>
                 <td>{{$post->title}}</td>
                 <td>{{$post->getFormattedDate('created_at')}}</td>
-                <td><a href="{{route('admin.posts.show', $post->id)}}" class="btn btn-primary">Vai</a></td>
+                <td class="d-flex">
+                  <a href="{{route('admin.posts.show', $post->id)}}" class="btn btn-primary">Vai</a>
+                  <a href="{{route('admin.posts.edit', $post->id)}}" class="btn btn-warning ml-2">Modifica</a>
+                  <form action="{{route('admin.posts.destroy', $post->id)}}" method="post">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit" class="btn btn-danger ml-2">Elimina</button>                  
+                  </form>
+                </td>
             </tr>
             @empty
                 <tr>
@@ -25,6 +37,9 @@
             @endforelse
         </tbody>
       </table>
+      <footer class="d-flex justify-content-end">
+        {{$posts->links()}}
+      </footer>
 </div>
     
 @endsection
