bbbee0df749d76e400ef1c07339533374c1f0192
mirkone1994 <mirkochiappori@gmail.com>
2021-10-19 18:23:20 +0200

    Corrections

--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -1,8 +1,9 @@
 @extends('layouts.app')
 @section('content')
     <div class="container">
-        <header>
+        <header class="d-flex justify-content-between align-items-center">
             <h1>Modifica Post</h1>
+            <a href="{{url()->previous()}}" class="btn btn-secondary">Indietro</a>
         </header>
         <section id="form">
             @include('includes.admin.posts.form')
