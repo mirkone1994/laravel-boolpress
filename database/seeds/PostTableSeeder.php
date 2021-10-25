a2cd7889540b69f778686ccebdfe1f135395050a
mirkone1994 <mirkochiappori@gmail.com>
2021-10-25 15:12:41 +0200

    added random categories

--- database/seeds/PostTableSeeder.php
+++ database/seeds/PostTableSeeder.php
@@ -4,6 +4,8 @@ use App\Models\Post;
 use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
 use Faker\Generator as Faker;
+use App\Models\Category;
+use Illuminate\Support\Arr;
 
 class PostTableSeeder extends Seeder
 {
@@ -14,8 +16,11 @@ class PostTableSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
+        $categories_id = Category::select('id')->pluck('id')->toArray();
+        
         for($i = 0; $i < 50; $i++){
             $post = new Post();
+            $post->category_id = Arr::random($categories_id);
             $post->title = $faker->text(50);
             $post->content = $faker->paragraphs(2, true);
             $post->image = $faker->imageUrl(360, 360);
--- resources/views/includes/admin/posts/form.blade.php
+++ resources/views/includes/admin/posts/form.blade.php
@@ -22,7 +22,7 @@
     <div class="form-group">
       <label for="category_id">Categoria</label>
       <select class="form-control" id="category_id" name="category_id">
-        <option>Nessuna categoria</option>
+        <option value="">Nessuna categoria</option>
         @foreach ($categories as $category)
         <option @if(old('category_id', $post->category_id) == $category->id) selected @endif value="{{$category->id}}">{{$category->name}}</option> 
         @endforeach
